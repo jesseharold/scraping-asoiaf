b8f0cf8ad5331744b61e130cd89da1f2ddca81cd
Jesse Harold <jesse.harold@leafgroup.com>
2018-04-24 17:40:16 -0700

    add simple visualization of books and chapters

--- create_js.py
+++ create_js.py
@@ -1,25 +1,45 @@
 import sqlite3
 
-fileh = open("pub/asoiaf_nodes_test.js", "w+")
-#fileh2 = open("pub/asoiaf_edges_test.js", "w+")
+fileh = open("pub/asoiaf_characters.js", "w+")
+fileh2 = open("pub/asoiaf_chapters.js", "w+")
 
 conn = sqlite3.connect('data.sqlite')
 cur = conn.cursor()
 
-fileh.write("var nodes = new vis.DataSet([")
-#fileh2.write("var edges = new vis.DataSet([")
+fileh.write("var characters = [")
 
-cur.execute('SELECT * FROM Characters')
+cur.execute('SELECT id, name FROM Characters')
+rows = cur.fetchall()
+for char in rows:
+    name = char[1].replace("'", "\\'")
+    fileh.write("{id: " + str(char[0]) + ", name: '" + name + "'},\n")
+
+fileh.write("];")
+
+fileh2.write("var chapters = [")
+
+cur.execute('SELECT name, pov, book, position, characters FROM Chapters')
 rows = cur.fetchall()
 count = 0
-for char in rows:
-    name = char[2].replace("'", "\\'")
-    fileh.write("{id: " + str(count) + ", label: '" + name + "'},\n")
+for chap in rows:
+    name = chap[0].replace("'", "\\'")
+    characters = chap[4].split(",")
+    char_array = ""
+    for item in characters:
+        char_array = char_array + "'" + item.replace("'", "\\'") + "', "
+    char_array = char_array[:-2]
+
+    fileh2.write("{id: " + str(count)
+        + ", label: '" + name
+        + "', book: '" + chap[2]
+        + "', position: '" + str(chap[3])
+        + "',\ncharacters: ["
+        + char_array
+        + "]},\n")
     count = count + 1
 
-fileh.write("]);")
-#fileh2.write("]);")
+fileh2.write("];")
 
 fileh.close()
-#fileh2.close()
+fileh2.close()
 conn.close()
